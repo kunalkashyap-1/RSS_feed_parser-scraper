c2dda93b88ec130fb61067e647ae64dd4d0aa019
kunalkashyap-1 <kunal.kashyap.8775@gmail.com>
2023-02-24 23:51:35 +0530

    search scraper updated

--- scraper_controller/search_feeds.js
+++ scraper_controller/search_feeds.js
@@ -7,10 +7,10 @@ module.exports = async function (rss_link) {
     const $ = cheerio.load(response.data);
 
     const [titles, imageUrls, links, contents] = await Promise.all([
-        Promise.resolve($(".Mc7GB .EW1Mb").map((i, el) => $(el).text()).get()),
-        Promise.resolve($(".Mc7GB a>figure > div > img").map((i, el) => $(el).attr("src")).get()),
-        Promise.resolve($(".Mc7GB a").map((i, el) => $(el).attr("href")).get()),
-        Promise.resolve($(".Mc7GB p").map((i, el) => $(el).text()).get())
+        Promise.resolve($(".crmK8 .fHv_i").map((i, el) => $(el).text()).get()),
+        Promise.resolve($(".crmK8 a>figure > div > img").map((i, el) => $(el).attr("src")).get()),
+        Promise.resolve($(".crmK8 a").map((i, el) => $(el).attr("href")).get()),
+        Promise.resolve($(".crmK8 p").map((i, el) => $(el).text()).get())
     ]);
 
     const data = titles.map((title, i) => ({
@@ -20,6 +20,6 @@ module.exports = async function (rss_link) {
         link: links[i],
     }));
 
-    // console.log(data);
+    // console.log(titles);
     return { data };
 }
